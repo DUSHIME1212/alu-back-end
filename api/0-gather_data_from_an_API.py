57f048c8ffe1455124a2d7af5297feb5078dea71
Don Aime <118371312+DUSHIME1212@users.noreply.github.com>
2024-06-28 08:30:39 +0200

    fix: task 1

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,18 +11,14 @@ def get_employee_info(employee_id):
     """
     url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
     response = requests.get(url)
-    if response.status_code != 200:
-        raise Exception("Error fetching employee information")
     return response.json()
 
 def get_employee_todos(employee_id):
     """
     Get the TODO list of the employee by employee ID
     """
-    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
     response = requests.get(url)
-    if response.status_code != 200:
-        raise Exception("Error fetching TODO list")
     return response.json()
 
 def main(employee_id):
@@ -33,25 +29,16 @@ def main(employee_id):
     employee_name = employee.get("name")
 
     emp_todos = get_employee_todos(employee_id)
-    tasks = {todo.get("title"): todo.get("completed") for todo in emp_todos}
-
-    total_tasks = len(tasks)
-    completed_tasks = [title for title, completed in tasks.items() if completed]
+    total_tasks = len(emp_todos)
+    completed_tasks = [todo for todo in emp_todos if todo.get("completed")]
     completed_tasks_count = len(completed_tasks)
 
-    print(f"Employee {employee_name} is done with tasks"
-          f"({completed_tasks_count}/{total_tasks}):")
-    for title in completed_tasks:
-        print(f"\t {title}")
+    print(f"Employee {employee_name} is done with tasks({completed_tasks_count}/{total_tasks}):")
+    for todo in completed_tasks:
+        print(f"\t {todo.get('title')}")
 
 if __name__ == "__main__":
     if len(argv) > 1:
-        try:
-            employee_id = int(argv[1])
-            main(employee_id)
-        except ValueError:
-            print("Error: Employee ID must be an integer.")
-        except Exception as e:
-            print(f"Error: {e}")
+        main(int(argv[1]))
     else:
         print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
